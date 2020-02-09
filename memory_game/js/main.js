3c111152352805499f207e3e6c51c2b6ea10cbb5
Vivian Wan <vivianpswan@gmail.com>
2020-02-10 09:12:05 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("user flipped " + cardsInPlay[0]);
---
+++ memory_game/main.js
@@ -0,0 +1,16 @@
+/*
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay
+*/
+
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
