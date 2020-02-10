4accda16aba7842b88087dacdd44e222844b590d
Vivian Wan <vivianpswan@gmail.com>
2020-02-10 23:08:52 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,28 @@
 
+let cards = [ "queen", "queen", "king", "king" ];
 
-let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("user flipped " + cardsInPlay[0]);
+//function flipCard (cardID) {
+
+	let cardOne = cards[1];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	let cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+
+	for (var i = 0; i < cardsInPlay.length; ++i) {
+			if (cardsInPlay[0]===cardsInPlay[1]) {
+				alert ("You found a match!")
+			} else {
+				alert ("Sorry, try again.")
+			}
+		}
+		
+//	console.log ("User flipped " + cards[cardID]);	
+
+
+
+
--- memory_game/main.js
+++ memory_game/main.js
@@ -1,16 +1,41 @@
+
 /*
-cards = ["queen", "queen", "king", "king"];
-cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
 */
 
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = [ "queen", "queen", "king", "king" ];
+
+let cardsInPlay = [];
+
+//function flipCard (cardID) {
+
+/*
+	let cardOne = cards[1];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+*/
+
+	let cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+/*
+	for (var i = 0; i < cardsInPlay.length; ++i) {
+			if (cardsInPlay[0]===cardsInPlay[1]) {
+				alert ("You found a match!")
+			} else {
+				alert ("Sorry, try again.")
+			}
+		}
+		
+	console.log ("User flipped " + cards[cardID]);	
+}
+
+*/
+
+
+
+
